4e49d3d137d5bdb65ccefb1249fd1021c2f49da0
anshul <ansh0003@student.monash.edu>
2025-03-15 18:09:24 +1100

    testing

--- geoloc.py
+++ geoloc.py
@@ -1,5 +1,5 @@
 from PIL import Image
 
-img = Image.open(Macintosh HD/Users/jaryl/JJAAAD/helpp.png)
+img = Image.open("C:/Users/2ansg/OneDrive/Desktop/2025/Unihack/JJAAAD/JJAAAD/helpp.png")
 exif_data = img._getexif()
 
